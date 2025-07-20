153db9de74b5cdb4b447e81c815bd02a851e19d6
adhizsrm <adhizsrm@gmail.com>
2025-07-20 22:57:20 +0530

    Enhance UI and add draw card functionality with improved layout styling

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 let deckId;
+const cardsContainer = document.getElementById("cards");
+const newDeckBtn = document.getElementById("new-deck");
+const drawCardBtn = document.getElementById("draw-cards");
+
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       deckId = data.deck_id;
     });
 }
+
+newDeckBtn.addEventListener("click", handleClick);
+
+drawCardBtn.addEventListener("click", () => {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      for (let i = 0; i < 2; i++) {
+        cardsContainer.children[
+          i
+        ].innerHTML = `<img src=${data.cards[i].image} class="card">`;
+      }
+    });
+});
