2aa85b62179aac36d00159469b1e608fd86ddc58
adhizsrm <adhizsrm@gmail.com>
2025-07-20 23:07:27 +0530

    adding background image

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
+      console.log(data);
       deckId = data.deck_id;
     });
 }
