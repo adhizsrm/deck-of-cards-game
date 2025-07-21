8e6991fdecf54dc0f90ea3d08ada451e9ac23930
adhizsrm <adhizsrm@gmail.com>
2025-07-21 23:02:05 +0530

    Implement draw functionality and card winner logic with styled UI

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ let deckId;
 const cardsContainer = document.getElementById("cards");
 const newDeckBtn = document.getElementById("new-deck");
 const drawCardBtn = document.getElementById("draw-cards");
+const header = document.getElementById("header");
 
 function handleClick() {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
       deckId = data.deck_id;
     });
 }
@@ -23,5 +23,34 @@ drawCardBtn.addEventListener("click", () => {
           i
         ].innerHTML = `<img src=${data.cards[i].image} class="card">`;
       }
+      const winnerText = determineCardWinner(data.cards[0], data.cards[1]);
+      header.textContent = winnerText;
     });
 });
+
+function determineCardWinner(card1, card2) {
+  const valueOptions = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "JACK",
+    "QUEEN",
+    "KING",
+    "ACE",
+  ];
+  const card1ValueIndex = valueOptions.indexOf(card1.value);
+  const card2ValueIndex = valueOptions.indexOf(card2.value);
+  if (card1ValueIndex > card2ValueIndex) {
+    return "Card 1 wins!";
+  } else if (card1ValueIndex < card2ValueIndex) {
+    return "Card 2 wins!";
+  } else {
+    return "War!";
+  }
+}
